b4fdece245b4caebb7f87fc363b603d31673ad61
goodzlmn <goodzlmn@naver.com>
2019-07-04 23:27:11 +0900

    EX_Object1

---
+++ C6_Object1/CarTest.java
@@ -0,0 +1,27 @@
+package C6_Object1;
+
+class Car {
+	String color;
+	String gearType;
+	int door;
+	Car(){}
+	Car(String c, String g, int d){
+		color = c;
+		gearType = g;
+		door = d;
+	}
+}
+
+public class CarTest {
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.color = "white";
+		c1.gearType = "auto";
+		c1.door = 4;
+		
+		Car c2 = new Car("black", "auto", 2);
+		
+		System.out.println(c1.color + " " + c1.gearType + " " + c1.door);
+		System.out.println(c2.color + " " + c2.gearType + " " + c2.door);
+	}
+}
